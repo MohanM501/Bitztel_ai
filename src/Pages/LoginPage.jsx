d6d95dc08b438e39f42f6ea74615d58bdb065b1a
MohanM501 <mohananna501b@gmail.com>
2025-01-08 12:11:11 +0530

    added login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { Box, Button, Paper, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [loginCred,setLoginCred]=useState({email:"",password:""});
+ 
 
-export default LoginPage
+  const postFetchAPI=(data)=>{
+    let Jdata=JSON.stringify(data);
+     console.log(Jdata,"jsondata");
+      fetch(`bffapi.biztel.ai:8080/api/auth/login`,
+        {
+          method:"POST",
+          body:Jdata,
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        }
+      )
+      .then(resp=>resp.json())
+      .then(json=>console.log(json,"json post request success"))
+      .catch(error=>{
+        console.log("Fetch Post Error",error);
+        alert("Unsuccessful login fetch request");
+      })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginCred,"loginCred");
+    postFetchAPI(loginCred);
+
+  };
+  const handleChanges=(e)=>{
+    const {name,value}=e.target;
+    setLoginCred({...loginCred,[name]:value});
+  };
+  return (
+    <div>
+      <Paper
+        elevation={3}
+        sx={{ maxWidth: 400, margin: "auto", padding: 4, mt: 4 }}
+      >
+        <Box component="form" onSubmit={handleSubmit}>
+          <Box sx={{ m: "30px" }}>
+            <TextField
+              id="outlined-basic"
+              label="Enter your email"
+              variant="outlined"
+              name="email"
+              onChange={handleChanges}
+            />
+          </Box>
+          <Box>
+            <TextField
+              id="outlined-password-input"
+              type="password"
+              label="Enter password"
+              variant="outlined"
+              name="password"
+              autoComplete="current-password"
+              onChange={handleChanges}
+            />
+          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ fontSize: { xs: "10px", sm: "14px" }, mt: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Paper>
+    </div>
+  );
+};
+
+export default LoginPage;
